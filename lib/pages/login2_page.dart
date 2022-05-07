3197e3240041266483ddd772abfdef5927093ae1
Mandarina <idcareg.g@gmail.com>
2022-05-06 22:56:18 -0500

    Login2 2.3

--- lib/pages/login2_page.dart
+++ lib/pages/login2_page.dart
@@ -13,32 +13,54 @@ class Login2Page extends StatelessWidget {
           //Fondo
           Stack(
             children: [
-              Container(
-                height: 300,
-                width: 300,
-                decoration: BoxDecoration(
-                  color: Colors.pinkAccent,
-                  shape: BoxShape.circle,
-                  gradient: LinearGradient(
-                    begin: Alignment.bottomRight,
-                    end: Alignment.topLeft,
-                    colors: [
-                      Color(0xffD74286),
-                      Color(0xffE991B9),
-                    ],
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color(0xffD74286).withOpacity(0.7),
-                      blurRadius: 12,
-                      offset: const Offset(2, 2),
-                    ),
+              Positioned(
+                top: -150,
+                right: -150,
+                child: MyCircleWidget(
+                  radius: 200,
+                  colors: [
+                    Color(0xffD74286),
+                    Color(0xffE991B9),
                   ],
                 ),
               ),
             ],
           ),
           //Formulario
+        ],
+      ),
+    );
+  }
+}
+
+
+
+class MyCircleWidget extends StatelessWidget {
+
+  double radius;
+  List<Color> colors;
+
+  MyCircleWidget({required this.radius, required this.colors});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: radius * 2,
+      width: radius * 2,
+      decoration: BoxDecoration(
+        color: Colors.pinkAccent,
+        shape: BoxShape.circle,
+        gradient: LinearGradient(
+          begin: Alignment.bottomRight,
+          end: Alignment.topLeft,
+          colors: colors,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: colors[0].withOpacity(0.7),
+            blurRadius: 12,
+            offset: const Offset(2, 2),
+          ),
         ],
       ),
     );
