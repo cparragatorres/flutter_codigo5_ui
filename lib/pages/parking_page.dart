1e6d46a63767159549cf3589ee969a489270cf09
Mandarina <idcareg.g@gmail.com>
2022-05-07 10:42:13 -0500

    Parking 2

--- lib/pages/parking_page.dart
+++ lib/pages/parking_page.dart
@@ -6,16 +6,21 @@ class ParkingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print(MediaQuery.of(context).size.height);
+    print(MediaQuery.of(context).size.width);
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             Container(
-              height: 300,
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
               width: double.infinity,
-              color: Colors.red,
+              color: Color(0xff0A494E),
               child: SafeArea(
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -38,6 +43,9 @@ class ParkingPage extends StatelessWidget {
                                 fontWeight: FontWeight.w500,
                               ),
                             ),
+                            const SizedBox(
+                              width: 6.0,
+                            ),
                             Image.asset(
                               'assets/images/cloudy.png',
                               width: 22.0,
@@ -46,8 +54,29 @@ class ParkingPage extends StatelessWidget {
                         ),
                       ],
                     ),
-                    Text("asdsd"),
-                    Row(),
+                    Container(
+                      margin: const EdgeInsets.only(top: 22.0, bottom: 16),
+                      constraints: BoxConstraints(
+                        maxWidth: MediaQuery.of(context).size.width * 0.7,
+                      ),
+                      child: Text(
+                        "Let's find a place for you",
+                        style: GoogleFonts.montserrat(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 36.0),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextField(),
+                        ),
+                        Icon(
+                          Icons.filter_alt_outlined
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
