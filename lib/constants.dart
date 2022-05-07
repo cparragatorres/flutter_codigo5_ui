73786d9f24d7a111481dbe026ae9de6cb95a63a7
Mandarina <idcareg.g@gmail.com>
2022-05-06 20:08:11 -0500

    Init Login 1

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+
+import 'dart:ui';
+
+const kColorPrimaryLogin = Color(0xff1E1C2A);
+const kColorSecondaryLogin = Color(0xff00B761);
+const kColorTertiaryLogin = Color(0xff25283A);
---
+++ lib/pages/login1_page.dart
@@ -0,0 +1,49 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_codigo5_ui/constants.dart';
+
+class Login1Page extends StatelessWidget {
+  const Login1Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kColorPrimaryLogin,
+      body: Stack(
+        children: [
+          Stack(
+            children: [
+              Positioned(
+                left: 40,
+                top: -40,
+                child: Transform.rotate(
+                  angle: - pi / 3,
+                  child: Container(
+                    width: 200,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: kColorSecondaryLogin,
+                      borderRadius: BorderRadius.circular(13.0),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 10.0),
+              child: Column(
+                children: [
+                  Icon(Icons.home, color: Colors.white,),
+
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
