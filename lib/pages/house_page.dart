81feab96255a12823919bbff40de17cba4ec776a
Mandarina <idcareg.g@gmail.com>
2022-05-09 21:55:10 -0500

    House 4

--- lib/pages/house_page.dart
+++ lib/pages/house_page.dart
@@ -54,10 +54,9 @@ class HousePage extends StatelessWidget {
                         borderRadius: BorderRadius.circular(10.0),
                         boxShadow: [
                           BoxShadow(
-                            color: Colors.black.withOpacity(0.03),
-                            offset: const Offset(4, 4),
-                            blurRadius: 14.0
-                          ),
+                              color: Colors.black.withOpacity(0.03),
+                              offset: const Offset(4, 4),
+                              blurRadius: 14.0),
                         ],
                       ),
                       child: Stack(
@@ -75,10 +74,8 @@ class HousePage extends StatelessWidget {
                               decoration: BoxDecoration(
                                 color: Colors.redAccent,
                                 shape: BoxShape.circle,
-                                border: Border.all(
-                                  color: Colors.white,
-                                  width: 1.2
-                                ),
+                                border:
+                                    Border.all(color: Colors.white, width: 1.2),
                               ),
                             ),
                           ),
@@ -86,6 +83,42 @@ class HousePage extends StatelessWidget {
                       ),
                     ),
                   ],
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.04),
+                        blurRadius: 12,
+                        offset: const Offset(4, 4),
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      hintText: "Search",
+                      hintStyle: GoogleFonts.montserrat(
+                        color: Colors.black.withOpacity(0.4)
+                      ),
+                      suffixIcon: Icon(
+                        Icons.search,
+                        color: Colors.black.withOpacity(0.4),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
