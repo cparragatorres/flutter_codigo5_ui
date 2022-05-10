20eecaff5d096d34e5bf7cdd296468d557f03a39
Mandarina <idcareg.g@gmail.com>
2022-05-09 21:20:57 -0500

    House 2

--- lib/pages/house_page.dart
+++ lib/pages/house_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -8,35 +7,60 @@ class HousePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffF9FBFC),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Icon(Icons.location_on,),
-                          Text(
-                            "Location",
-                            style: GoogleFonts.montserrat(),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.location_on,
+                              color: Color(0xffC1D6F4),
+                              size: 14.0,
+                            ),
+                            Text(
+                              "Location",
+                              style: GoogleFonts.montserrat(
+                                fontSize: 12.0,
+                                color: Colors.black.withOpacity(0.45),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 4.0,
+                        ),
+                        Text(
+                          "Purbalingga, Jawa Tengah",
+                          style: GoogleFonts.montserrat(
+                            fontWeight: FontWeight.w600,
                           ),
-                        ],
+                        ),
+                      ],
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(12.0),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10.0)
                       ),
-                      Text(
-                        "Purbalingga, Jawa Tengah",
-                        style: GoogleFonts.montserrat(),
+                      child: Icon(
+                        Icons.notifications,
                       ),
-                    ],
-                  ),
-                  Icon(Icons.notifications,),
-                ],
-              ),
-            ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
